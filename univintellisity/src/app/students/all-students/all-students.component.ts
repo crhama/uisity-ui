bef014aa75bfb8a38b1c009dc1e50474738462d3
Charles Ramazany <cramazany@pwcusgov.com>
2019-08-05 13:53:19 -0400

    unsubscribe

--- univintellisity/src/app/students/all-students/all-students.component.ts
+++ univintellisity/src/app/students/all-students/all-students.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { Store, select } from '@ngrx/store';
+import { Observable } from 'rxjs';
 
 import * as stdVm from '../models/student-vm';
 import * as fromStudents from '../state/students.reducer'
 import { DataDisplayMode } from 'src/app/shared/models/enums';
 import * as studentsActions from "../state/students.action";
+
 
 
 @Component({
@@ -13,16 +15,14 @@ import * as studentsActions from "../state/students.action";
   styleUrls: ['./all-students.component.css']
 })
 export class AllStudentsComponent implements OnInit {
-  studentList: stdVm.StudentViewModel[];
+  studentList$: Observable<stdVm.StudentViewModel[]>;
   displayMode: DataDisplayMode;
   
   constructor(private store: Store<fromStudents.IState>) { }
 
   ngOnInit() {
       this.store.dispatch(new studentsActions.LoadAllStudents());
-      this.store.pipe(select(fromStudents.getAllStudents)).subscribe(
-        students => this.studentList = students
-      );
+      this.studentList$ = this.store.pipe(select(fromStudents.getAllStudents));
 
 
       this.store.pipe(select(fromStudents.getDataDisplayMode)).subscribe(
